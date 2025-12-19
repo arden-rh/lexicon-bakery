068b95a97c2deff86f06ed257866c2d0c43ba949
Arden <arden.haldorson@gmail.com>
2025-12-19 16:05:01 +0100

    created some mock data with products to be rendered on the Product page
    moved typography variables

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,14 @@
+import rawData from '../data/products.json';
+import type { ProductResponse, Product } from '../types/product.types';
+
+// Cast the imported JSON to your Interface
+const data = rawData as ProductResponse;
+
 const Products = () => {
     return (
         <section>
             <h2>Our Products</h2>
             <ul>
-                <li>Freshly Baked Bread</li>
                 <li>Assorted Pastries</li>
                 <li>Custom Cakes</li>
             </ul>
---
+++ src/types/product.types.ts
@@ -0,0 +1,25 @@
+export type Category = 'cupcake' | 'wedding';
+export type InStock = true | false ;
+
+export interface Product {
+  id: string;
+  title: string;
+  category: Category;
+  price: number;
+  image: string;
+  description: string;
+  rating: number;
+  inStock: InStock;
+  features?: string[]; // Optional array for things like "Gluten-Free"
+}
+
+export interface ProductResponse {
+  metadata: {
+    generatedAt: string;
+    totalItems: number;
+    categories: Category[];
+    currency: string;
+    storeStatus: 'open' | 'closed';
+  };
+  products: Product[];
+}
