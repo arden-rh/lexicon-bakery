b6e200a540db812e21ab18c69e4a4a649ed51d79
Arden <arden.haldorson@gmail.com>
2025-12-21 16:07:35 +0100

    styling of grid and cards

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import styles from '@/components/ProductCard/ProductCard.module.scss';
-import type { Product } from '@/types/product.types';
+import React from "react";
+import styles from "@/components/ProductCard/ProductCard.module.scss";
+import type { Product } from "@/types/product.types";
 
 interface ProductCardProps {
-    product: Product
-};
+	product: Product;
+}
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-    return (
-        <div className={styles.card} key={product.id}>
-            <img src={product.image} alt={product.title} className={styles.image} />
-            <h3 className={styles.title}>{product.title}</h3>
-            <p className={styles.price}>${product.price.toFixed(2)}</p>
-            <p className={styles.description}>{product.description}</p>
-        </div>
-    );
-}
+	return (
+		<div className={styles.card} key={product.id}>
+			<img
+				src={product.image}
+				alt={product.title}
+				className={styles.image}
+			/>
+			<div className={styles.content}>
+				<h4 className={styles.title}>{product.title}</h4>
+				<p className={styles.price}>${product.price.toFixed(2)}</p>
+				<p className={styles.description}>{product.description}</p>
+			</div>
+		</div>
+	);
+};
